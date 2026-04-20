f67b0b6408436d62536b0d3cb25ef3363ad1bdb6
Tony Zerrer <tonyzerrer@gmail.com>
2026-04-19 20:41:56 -0500

    Fix sitemap site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://student-performance-toolkit-app.vercel.app',
+  site: 'https://student-performance-toolkit-v1.vercel.app',
   integrations: [sitemap()],
 });
