b082dc2fc2006e53fc3d0a7db5ad7be87325b42e
Tony Zerrer <tonyzerrer@gmail.com>
2026-04-20 18:12:46 -0500

    Update site URL to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://student-performance-toolkit-6hgn.vercel.app',
+  site: 'https://gradetoolkit.com',
   integrations: [sitemap()],
 });
