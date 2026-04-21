5f3ec889c7a6b007a0d5dc3069a6635e7a51de9a
Tony Zerrer <tonyzerrer@gmail.com>
2026-04-20 20:47:24 -0500

    Make homepage use shared tools and guides data

---
+++ src/data/site.ts
@@ -0,0 +1,6 @@
+export const site = {
+  name: "Grade Toolkit",
+  url: "https://gradetoolkit.com",
+  description:
+    "Free tools and guides for calculating grades, finals, GPA, and what you need to pass.",
+};
