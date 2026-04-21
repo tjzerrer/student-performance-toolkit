52ce3460bbf8197c553deb5dbf690ee652e34e97
Anthony Zerrer <tonyzerrer@gmail.com>
2026-04-21 12:23:20 -0500

    Add article: what is a good GPA

--- src/data/guides.ts
+++ src/data/guides.ts
@@ -2,4 +2,5 @@ export const guides = [
   { title: "How to Calculate Your Grade", href: "/how-to-calculate-your-grade" },
   { title: "What Grade Do I Need to Pass a Class?", href: "/what-grade-do-i-need-to-pass-a-class" },
   { title: "How Finals Affect Your Grade", href: "/how-finals-affect-your-grade" },
+  { title: "What Is a Good GPA?", href: "/what-is-a-good-gpa" },
 ];
