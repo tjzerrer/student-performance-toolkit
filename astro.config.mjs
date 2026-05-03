9a3e4b7631bd098067aff45fa958727820a79f92
Tony Zerrer <tonyzerrer@gmail.com>
2026-05-03 07:58:54 -0500

    Add sitemap XML alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import { copyFile } from 'node:fs/promises';
 import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   site: 'https://gradetoolkit.com',
   trailingSlash: 'always',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(),
+    {
+      name: 'sitemap-xml-alias',
+      hooks: {
+        'astro:build:done': async ({ dir, logger }) => {
+          await copyFile(new URL('sitemap-index.xml', dir), new URL('sitemap.xml', dir));
+          logger.info('Created sitemap.xml alias for sitemap-index.xml');
+        },
+      },
+    },
+  ],
   vite: {
     resolve: {
       alias: {
